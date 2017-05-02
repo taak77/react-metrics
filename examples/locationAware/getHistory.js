685405d5c627dd58e9e0465f8d0a40913aaf6fa2
Takashi Aoki <taak77@gmail.com>
2017-05-02 10:44:33 -0700

    rollback

--- examples/locationAware/getHistory.js
+++
@@ -1,8 +0,0 @@
-export default function getHistory(props = {}, context = {}) {
-    const {router = {}} = context;
-    const history = props.history || router.history;
-    if (history && typeof history.listen === "function") {
-        return history;
-    }
-    return null;
-}
--- src/react/locationEquals.js
+++ src/react/locationEquals.js
@@ -10,8 +10,6 @@ export default function locationEquals(a, b) {
     return (
         a.pathname === b.pathname &&
         a.search === b.search &&
-        a.hash === b.hash &&
-        a.key === b.key &&
         deepEqual(a.state, b.state)
     );
 }
